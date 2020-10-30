a801b72067b169887d09d4f4654d80c22a43a5e5
Igor Bayerl <bayerl.igor@gmail.com>
2020-10-29 22:28:42 -0300

    creating the project

---
+++ fileOrganizer.py
@@ -0,0 +1,224 @@
+import time
+import os
+import json
+import shutil
+from datetime import datetime
+from pathlib import Path
+from time import gmtime, strftime
+
+class FileOrganizer:
+    extensions = [
+        'jpg', 
+        'jpeg', 
+        'png', 
+        'ico'
+    ]  
+
+    def move_file(self, filename):
+        if filename != 'fileOrganizer.ico':
+            i = 1
+            extension = 'noname'
+            new_name = filename
+            try:
+                file_Name ,extension = os.path.splitext(filename)
+                print(file_Name)
+            except Exception :
+                extension = 'noname'
+
+            folder_destination_path = self.extensions_folders[extension]
+
+            print(folder_destination_path)
+
+            now = datetime.now()
+            year = now.strftime("%Y")
+
+            folderOperation = os.getcwd()
+
+            year_exists = False
+            for folder_name in os.listdir(self.extensions_folders[extension]):
+                if folder_name == year:
+                    folder_destination_path = self.extensions_folders[extension] + "/" +year
+                    year_exists = True
+                    
+            if not year_exists:
+                os.mkdir(self.extensions_folders[extension] + "/" + year)
+                folder_destination_path = self.extensions_folders[extension] + "/" + year
+
+            file_exists = os.path.isfile(folder_destination_path + "/" + new_name)
+            while file_exists:
+                i += 1
+                new_name = os.path.splitext(folderOperation + '/' + filename)[0] + str(i) + os.path.splitext(folderOperation + '/' + filename)[1]
+                new_name = new_name.split("/")
+                file_exists = os.path.isfile(folder_destination_path + "/" + new_name)
+            src = folderOperation + "/" + filename
+
+            new_name = folder_destination_path + "/" + new_name
+            os.rename(src, new_name)
+
+    def organize(self):
+        #constroi array com arquivos da pasta
+        files = [
+            filename for filename in os.listdir('.')
+                if os.path.isfile(filename) and any(filename.lower().endswith('.' + ext.lower()) for ext in self.extensions)
+        ]
+
+        print(files)
+        for filename in files:
+            self.move_file(filename)
+
+    extensions_folders = {
+    #No name
+        'noname' : "C:/Users/playb/Documents/Uncategorized",
+    #Audio
+        # '.aif' : "C:/Users/playb/Music",
+        # '.cda' : "C:/Users/playb/Music",
+        # '.mid' : "C:/Users/playb/Music",
+        # '.midi' : "C:/Users/playb/Music",
+        # '.mp3' : "C:/Users/playb/Music",
+        # '.mpa' : "C:/Users/playb/Music",
+        # '.ogg' : "C:/Users/playb/Music",
+        # '.wav' : "C:/Users/playb/Music",
+        # '.wma' : "C:/Users/playb/Music",
+        # '.wpl' : "C:/Users/playb/Music",
+        # '.m3u' : "C:/Users/playb/Music",
+    #Text
+        # '.txt' : "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.doc' : "C:/Users/playb/Documents/DocumentosDeTexto/Word",
+        # '.docx' : "C:/Users/playb/Documents/DocumentosDeTexto/Word",
+        # '.odt ' : "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.pdf': "C:/Users/playb/Documents/DocumentosDeTexto/PDF",
+        # '.rtf': "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.tex': "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.wks ': "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.wps': "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+        # '.wpd': "C:/Users/playb/Documents/DocumentosDeTexto/TextFiles",
+    #Video
+        # '.3g2': "C:/Users/playb/Videos",
+        # '.3gp': "C:/Users/playb/Videos",
+        # '.avi': "C:/Users/playb/Videos",
+        # '.flv': "C:/Users/playb/Videos",
+        # '.h264': "C:/Users/playb/Videos",
+        # '.m4v': "C:/Users/playb/Videos",
+        # '.mkv': "C:/Users/playb/Videos",
+        # '.mov': "C:/Users/playb/Videos",
+        # '.mp4': "C:/Users/playb/Videos",
+        # '.mpg': "C:/Users/playb/Videos",
+        # '.mpeg': "C:/Users/playb/Videos",
+        # '.rm': "C:/Users/playb/Videos",
+        # '.swf': "C:/Users/playb/Videos",
+        # '.vob': "C:/Users/playb/Videos",
+        # '.wmv': "C:/Users/playb/Videos",
+    #Images
+        # '.ai': "C:/Users/playb/Pictures",
+        # '.bmp': "C:/Users/playb/Pictures",
+        # '.gif': "C:/Users/playb/Pictures",
+        '.ico': "C:/Icons",
+        '.jpg': "C:/www/FileOrganizer/Testes/Pictures",
+        '.jpeg': "C:/www/FileOrganizer/Testes/Pictures",
+        '.png': "C:/www/FileOrganizer/Testes/Pictures",
+        # '.ps': "C:/Users/playb/Pictures",
+        # '.psd': "C:/Users/playb/Pictures",
+        # '.svg': "C:/Users/playb/Pictures/SVG",
+        # '.tif': "C:/Users/playb/Pictures",
+        # '.tiff': "C:/Users/playb/Pictures",
+        # '.CR2': "C:/Users/playb/Pictures",
+    #Internet
+        # '.asp': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.aspx': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.cer': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.cfm': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.cgi': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.pl': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.css': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.htm': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.js': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.jsp': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.part': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.php': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.rss': "C:/Users/playb/Desktop/kalle/Other/Internet",
+        # '.xhtml': "C:/Users/playb/Desktop/kalle/Other/Internet",
+    #Compressed
+        # '.7z': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.arj': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.deb': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.pkg': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.rar': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.rpm': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.tar.gz': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.z': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+        # '.zip': "C:/Users/playb/Desktop/kalle/Other/Compressed",
+    #Disc
+        # '.bin': "C:/Users/playb/Desktop/kalle/Other/Disc",
+        # '.dmg': "C:/Users/playb/Desktop/kalle/Other/Disc",
+        # '.iso': "C:/Users/playb/Desktop/kalle/Other/Disc",
+        # '.toast': "C:/Users/playb/Desktop/kalle/Other/Disc",
+        # '.vcd': "C:/Users/playb/Desktop/kalle/Other/Disc",
+    #Data
+        # '.csv': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.dat': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.db': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.dbf': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.log': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.mdb': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.sav': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.sql': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.tar': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.xml': "C:/Users/playb/Desktop/kalle/Programming/Database",
+        # '.json': "C:/Users/playb/Desktop/kalle/Programming/Database",
+    #Executables
+        # '.apk': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.bat': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.com': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.exe': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.gadget': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.jar': "C:/Users/playb/Desktop/kalle/Other/Executables",
+        # '.wsf': "C:/Users/playb/Desktop/kalle/Other/Executables",
+    #Fonts
+        # '.fnt': "C:/Users/playb/Desktop/kalle/Other/Fonts",
+        # '.fon': "C:/Users/playb/Desktop/kalle/Other/Fonts",
+        # '.otf': "C:/Users/playb/Desktop/kalle/Other/Fonts",
+        # '.ttf': "C:/Users/playb/Desktop/kalle/Other/Fonts",
+    #Presentations
+        # '.key': "C:/Users/playb/Desktop/kalle/Text/Presentations",
+        # '.odp': "C:/Users/playb/Desktop/kalle/Text/Presentations",
+        # '.pps': "C:/Users/playb/Desktop/kalle/Text/Presentations",
+        # '.ppt': "C:/Users/playb/Desktop/kalle/Text/Presentations",
+        # '.pptx': "C:/Users/playb/Desktop/kalle/Text/Presentations",
+    #Programming
+        # '.c': "C:/Users/playb/Desktop/kalle/Programming/C&C++",
+        # '.class': "C:/Users/playb/Desktop/kalle/Programming/Java",
+        # '.dart': "C:/Users/playb/Desktop/kalle/Programming/Dart",
+        # '.py': "C:/Users/playb/Desktop/kalle/Programming/Python",
+        # '.sh': "C:/Users/playb/Desktop/kalle/Programming/Shell",
+        # '.swift': "C:/Users/playb/Desktop/kalle/Programming/Swift",
+        # '.html': "C:/Users/playb/Desktop/kalle/Programming/C&C++",
+        # '.h': "C:/Users/playb/Desktop/kalle/Programming/C&C++",
+    #Spreadsheets
+        # '.ods' : "C:/Users/playb/Desktop/kalle/Text/Microsoft/Excel",
+        # '.xlr' : "C:/Users/playb/Desktop/kalle/Text/Microsoft/Excel",
+        # '.xls' : "C:/Users/playb/Desktop/kalle/Text/Microsoft/Excel",
+        # '.xlsx' : "C:/Users/playb/Desktop/kalle/Text/Microsoft/Excel",
+    #System
+        # '.bak' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.cab' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.cfg' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.cpl' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.cur' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.dll' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.dmp' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.drv' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.icns' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.ico' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.ini' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.lnk' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.msi' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.sys' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+        # '.tmp' : "C:/Users/playb/Desktop/kalle/Text/Other/System",
+}
+
+
+FO = FileOrganizer()
+FO.organize()
+
+
+# pyinstaller -w -F fileOrganizer.py
